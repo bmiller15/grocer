ccebe3a802f74c70e2d2b9cca26bc954aa9aa3b2
bmiller15 <bmiller15@apu.edu>
2017-10-27 19:32:09 -0700

    WelcomeScreen and App.js working

--- grocer/App.js
+++ grocer/App.js
@@ -1,11 +1,32 @@
+/*
+  Brenden Miller
+  Grocer
+  CS 495
+  Start Date: October 27, 2017
+*/
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// npm install --save react-navigation
+import { TabNavigator } from 'react-navigation';
+
+// Screen imports
+import AddNewScreen from './src/screens/AddNewScreen';
+import AuthScreen from './src/screens/AuthScreen';
+import GroceryListScreen from './src/screens/GroceryListScreen';
+import RecipesScreen from './src/screens/RecipesScreen';
+import SwipeScreen from './src/screens/SwipeScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = TabNavigator({
+      AuthScreen: { screen: AuthScreen },
+      
+    })
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
       </View>
     );
   }
@@ -14,8 +35,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
