4c689b55b30093c2b5a0bc3a301dbe81152dbe58
bmiller15 <bmiller15@apu.edu>
2017-10-27 19:33:29 -0700

    WelcomeScreen

---
+++ grocer/src/screens/WelcomeScreen.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+//  WelcomeScreen
+/*
+      Introduction to using Grocery
+      
+*/
+class WelcomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default WelcomeScreen;
