2a365031c05c46c677efbad8340a448ac21efaf6
bmiller15 <bmiller15@apu.edu>
2017-10-27 22:53:49 -0700

    starting point

--- grocer/App.js
+++ grocer/App.js
@@ -6,7 +6,7 @@
 */
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 // npm install --save react-navigation
 import { TabNavigator } from 'react-navigation';
 
@@ -16,17 +16,36 @@ import AuthScreen from './src/screens/AuthScreen';
 import GroceryListScreen from './src/screens/GroceryListScreen';
 import RecipesScreen from './src/screens/RecipesScreen';
 import SwipeScreen from './src/screens/SwipeScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+// Possibly add a settings?
+// Also possiblilty of having a social feed and send grocery list through texts
+// Have grocery items that are marked as permenitly had (ie. salt)
 
+// App.js (main file)
 export default class App extends React.Component {
   render() {
+    // Bottom navigation
     const MainNavigator = TabNavigator({
+      // First two will become skipped if signed in previously(autohydrate?)
+      // Possibly make WelcomeScreen only play once?
+      // No FB intergration, just doing sign in
+      WelcomeScreen: { screen: WelcomeScreen },
       AuthScreen: { screen: AuthScreen },
-      
-    })
+      main: {
+        // main navigation: Swipe, Grocery, Recipes, Add
+        screen: TabNavigator({
+          SwipeScreen: { screen: SwipeScreen },
+          GroceryListScreen: { screen: GroceryListScreen },
+          RecipesScreen: { screen: RecipesScreen },
+          AddNewScreen: { screen: AddNewScreen }
+        })
+      }
+
+    });
 
     return (
       <View style={styles.container}>
-
+        <MainNavigator />
       </View>
     );
   }
